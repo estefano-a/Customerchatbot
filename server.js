408e835d0b33475f87498b5a3a6c5fc583194c18
Dhuang50 <144459270+Dhuang50@users.noreply.github.com>
2024-05-03 21:42:20 -0400

    Update server.js

--- server.js
+++ server.js
@@ -82,7 +82,7 @@ http.createServer(function (req, res) {
     res.writeHead(
       200,
       {'Content-Type': 'text/html'},
-      {"Access-Control-Allow-Origin": "localhost:5500"},
+      {"Access-Control-Allow-Origin": "*"},
     )
     switch (body.request) {
       case "addUser":
