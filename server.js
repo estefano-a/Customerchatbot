4eb176acb9c3182939e39a7f70e84cdecb8c4300
Dhuang50 <144459270+Dhuang50@users.noreply.github.com>
2024-05-04 13:11:36 -0400

    Update server.js

--- server.js
+++ server.js
@@ -78,7 +78,6 @@ http.createServer(function (req, res) {
     body += chunk.toString()
   })
   req.on('end', async () => {
-    console.log(body)
     body = JSON.parse(body)
     res.writeHead(
       200,
