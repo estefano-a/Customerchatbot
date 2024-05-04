56f52ab65a56a77774b67abcc387eefa0f3f5bac
Dhuang50 <144459270+Dhuang50@users.noreply.github.com>
2024-05-03 22:30:05 -0400

    Update server.js

--- server.js
+++ server.js
@@ -73,13 +73,12 @@ async function addMessage(name, message, recipient) {
 }
 
 http.createServer(function (req, res) {
-  let body = ""
+  let body = "{"
   req.on('data', chunk => {
     body += chunk.toString()
   })
   req.on('end', async () => {
     body = JSON.parse(body)
-    console.log(body)
     res.writeHead(
       200,
       {'Content-Type': 'text/html'},
