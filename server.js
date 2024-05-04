90e3c21d128c3995202420aa28a47c48376490d1
Dhuang50 <144459270+Dhuang50@users.noreply.github.com>
2024-05-03 21:24:08 -0400

    Update server.js

--- server.js
+++ server.js
@@ -82,7 +82,7 @@ http.createServer(function (req, res) {
     res.writeHead(
       200,
       {'Content-Type': 'text/html'},
-      {"Access-Control-Allow-Origin": 'https://247teach.org'},
+      {"Access-Control-Allow-Origin": '*,'},
     )
     switch (body.request) {
       case "addUser":
