0fdcfb28e3ce9ac93ed3f86908195c425e6367f1
Dhuang50 <144459270+Dhuang50@users.noreply.github.com>
2024-05-08 20:24:20 -0400

    Update exports-service.js

--- front-end/service-representative/js/exports-service.js
+++ front-end/service-representative/js/exports-service.js
@@ -109,6 +109,7 @@ function requestMessagesAtTime(session, name) {
     section.innerHTML = "";
     section.classList.remove("current")
     section.classList.add(`s${session}`)
+    console.log("Received response:", this.responseText);
     if (this.responseText && this.responseText.trim().startsWith('{')) {
       try {
         const messages = JSON.parse(this.responseText);
