6d155382e5d5b1758edea4d0ecc261d35c5a927a
Estefano Ascencio <145227512+estefano-a@users.noreply.github.com>
2024-07-13 13:54:57 -0400

    Update server.js

--- server.js
+++ server.js
@@ -164,7 +164,27 @@ http
               feedbackText = "Our user gave a 👎 on Rebecca's performance.";
             }
 
+            if (window.getSelection) {
+              return window.getSelection().toString();
+            } else if (
+              document.selection &&
+              document.selection.type != "Control"
+            ) {
+              return document.selection.createRange().text;
+            }
+            return "";
+
             try {
+              if (window.getSelection) {
+                return window.getSelection().toString();
+              } else if (
+                document.selection &&
+                document.selection.type != "Control"
+              ) {
+                return document.selection.createRange().text;
+              }
+              return "";
+
               const latestMessage = await getLatestMessage(body.name);
               const text = latestMessage
                 ? `${feedbackText}\nLatest response from Rebecca: ${latestMessage}`
