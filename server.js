87f46e059fef4288bcdedce01c7f6d9bc8ea15c9
erlacla <71285158+erlacla@users.noreply.github.com>
2024-08-29 10:07:25 -0700

    Update server.js add ws require

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const { MongoClient } = require('mongodb');
 const { OpenAI } = require('openai');
 const { App } = require('@slack/bolt');
+const WebSocket = require('ws');
 const fs = require('fs');
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 const port = process.env.PORT || 10000;
